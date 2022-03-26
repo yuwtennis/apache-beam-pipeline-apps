1680738bdd8e161dd7149a2eace67d0927ada175
Yu Watanabe <yu.w.tennis@gmail.com>
2022-03-27 00:55:32 +0900

    Add note about split vector.

--- scripts/mongodb/createUser.js
+++ scripts/mongodb/createUser.js
@@ -1,8 +1,10 @@
-db = db.getSiblingDB('YOURDB')
+db = db.getSiblingDB('YOURDB') ;
+// clusterManager required to use splitVector
+// https://github.com/apache/beam/blob/master/sdks/java/io/mongodb/src/main/java/org/apache/beam/sdk/io/mongodb/MongoDbIO.java#L484
 db.createUser(
   {
     user: "YOURUSER",
     pwd:  passwordPrompt(),
     roles: [ { role: "readWrite", db: "YOURDB" }, {role: "clusterManager", db: "admin"}]
   }
-)
+) ;
